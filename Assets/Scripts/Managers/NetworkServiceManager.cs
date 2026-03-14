de401b856373fa519d7e474acf5b97992f90d978
enzo405 <enzo.chaboisseau@gmail.com>
2026-03-14 01:52:33 +0100

    Add MainMenu & LoadingScene design

--- Assets/Scripts/Managers/NetworkServiceManager.cs
+++ Assets/Scripts/Managers/NetworkServiceManager.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(ConfigApiService))]
 public class NetworkServiceManager : MonoBehaviour
 {
+    private static WaitForSeconds _waitForSeconds0_5 = new WaitForSeconds(0.5f);
+
     public static NetworkServiceManager Instance { get; private set; }
     public bool IsReady { get; private set; } = false;
     private PlayerApiService _playerService;
@@ -41,16 +43,17 @@ public class NetworkServiceManager : MonoBehaviour
     {
         string deviceId = SystemInfo.deviceUniqueIdentifier;
 
-        loadingUI.SetStatus("Connexion au serveur...", 0f);
         // 1 — Create or retrieve player
+        loadingUI.SetProgress("Connexion au serveur...", 0f);
         yield return StartCoroutine(_playerService.CreateOrGetPlayer(
             deviceId,
             onSuccess: player => Debug.Log($"[Network] Player initialized: {player.DeviceId}"),
             onError: error => Debug.LogError($"[Network] Player init failed with DeviceId {deviceId}: {error}")
         ));
+        yield return _waitForSeconds0_5;
 
         // 2 - Fetch Config
-        loadingUI.SetStatus("Chargement de la configuration...", 0.33f);
+        loadingUI.SetProgress("Chargement de la configuration...", 0.33f);
         yield return StartCoroutine(_configApiService.FetchDefeatConditions(
             onSuccess: defeatConditions => Debug.Log($"[Network] Fetched and initialized defeat conditions: Motivation={defeatConditions.Motivation.Min}/ {defeatConditions.Motivation.Max}; Stress={defeatConditions.Stress.Min}/ {defeatConditions.Stress.Max}; Performance={defeatConditions.Performance.Min}/ {defeatConditions.Performance.Max}; Turnover={defeatConditions.Turnover.Min}/ {defeatConditions.Turnover.Max}"),
             onError: error => Debug.LogError($"[Network] FetchDefeatConditions init failed: {error}")
@@ -65,18 +68,20 @@ public class NetworkServiceManager : MonoBehaviour
             onSuccess: statsInit => Debug.Log($"[Network] Fetched and initialized initial stats: InitialMotivation={statsInit.InitialMotivation}; InitialPerformance={statsInit.InitialPerformance}; InitialStress={statsInit.InitialStress}; InitialTurnover={statsInit.InitialTurnover}"),
             onError: error => Debug.LogError($"[Network] FetchStatsInit init failed: {error}")
         ));
+        yield return _waitForSeconds0_5;
+
 
         // 3 - Initialize Cards
-        loadingUI.SetStatus("Chargement des cartes...", 0.66f);
+        loadingUI.SetProgress("Chargement des cartes...", 0.66f);
         yield return StartCoroutine(_cardService.FetchAllCards(
                onSuccess: cards => Debug.Log($"[Network] Fetched and initialized {cards.Count} cards."),
                onError: error => Debug.LogError($"[Network] Card init failed: {error}")
            ));
 
         Debug.Log("[Network] Initialization complete.");
-        loadingUI.SetStatus("Prêt !", 1f);
+        loadingUI.SetProgress("Prêt !", 1f);
 
-        yield return new WaitForSeconds(1f);
+        yield return _waitForSeconds0_5;
         loadingUI = null;
         SceneManager.LoadScene("MainMenu");
     }
